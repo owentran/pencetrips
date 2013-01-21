dfca6e9e81a3e73a80d95d1c99cf9245f6e55565
Owen Tran <owen@pointshound.com>
2013-01-21 08:42:50 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Penceworld::Application.config.secret_token = '2a2fd660e5fc9ea10470afbd4951788cd8557e2a8470db9baf55ee383c6bf14d689972661f01d43f5bee1345306129acfcffb3c75ab2825171a6099b08501579'
